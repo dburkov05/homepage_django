f0ef81b8e0031dd6e4fb7e49e66a11fd2f8e53eb
Denis_Burkov <dburkov05@bk.ru>
2022-09-24 22:41:02 +0500

    replace templates and add to .gitignore python cache

--- homepage_project/settings.py
+++ homepage_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,7 +65,6 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
     },
 ]
 
