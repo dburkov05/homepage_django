ba1e0f6c6f5ffa23901d231a8c61604ff11843a8
Denis_Burkov <dburkov05@bk.ru>
2022-09-24 22:00:00 +0500

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase, SimpleTestCase
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
